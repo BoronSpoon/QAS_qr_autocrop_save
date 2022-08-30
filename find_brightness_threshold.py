bafa5d3e68425485d2556158b8188c69be4fe7df
rayanti <rayanticlimatic@gmail.com>
2022-08-30 15:40:02 +0900

    add cap settiing to find brightness threshold

--- find_brightness_threshold.py
+++ find_brightness_threshold.py
@@ -4,6 +4,9 @@ import numpy as np
 # finds brightness threshold to detect the on/off of microscope light
 # the threshold uses mean intensity of every pixel
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FPS, 60)
 min_ = 0
 max_ = 1e9
 for i in range(10000):

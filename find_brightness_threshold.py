cb32ab9cfff21dc8a91a153199d915ff5197c112
rayanti <rayanticlimatic@gmail.com>
2022-08-30 15:52:59 +0900

    minor debug

--- find_brightness_threshold.py
+++ find_brightness_threshold.py
@@ -7,13 +7,25 @@ cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 cap.set(cv2.CAP_PROP_FPS, 60)
-min_ = 1e9
-max_ = 0
+mean_min_ = 1e9
+mean_max_ = 0
+min_min_ = 1e9
+min_max_ = 0
+max_min_ = 1e9
+max_max_ = 0
 for i in range(10000):
     for j in range(100):
         ret, frame = cap.read()
-        intensity = np.mean(frame)
-        min_ = min(intensity, min_)
-        max_ = max(intensity, max_)
-    print(f"min, max = {min_}, {max_}")
+        mean_min_ = min(np.mean(frame), mean_min_)
+        mean_max_ = max(np.mean(frame), mean_max_)
+        min_min_ = min(np.min(frame), min_min_)
+        min_max_ = max(np.min(frame), min_max_)
+        max_min_ = min(np.max(frame), max_min_)
+        max_max_ = max(np.max(frame), max_max_)
+    print(f"(mean) min,max={mean_min_}, {mean_max_}, (min) min,max={min_min_}, {min_max_}, (max) min,max={max_min_}, {max_max_}")
     
+# when pc is off: 
+# when light is off: (mean) min,max=12.984489776234568, 149.94212593235596, (min) min,max=0, 0, (max) min,max=255, 255
+# when light is off and sample is on:
+# when light is on:
+# when light is on and sample is on:

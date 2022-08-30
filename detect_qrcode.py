5fad7c19542e25c6d8f632f9286c74f0faba8f7e
BoronSpoon <40061757+BoronSpoon@users.noreply.github.com>
2022-08-31 00:33:02 +0900

    fixing angle

--- detect_qrcode.py
+++ detect_qrcode.py
@@ -132,10 +132,10 @@ class Detect():
             centers = [self.corner_qr_dict[self.device][i]["bbox_center"] for i in range(2)]
             x_pos = [self.corner_qr_dict[self.device][i]["x_pos"] for i in range(2)]
             y_pos = [self.corner_qr_dict[self.device][i]["y_pos"] for i in range(2)]
-            dx_pos = x_pos[0] - x_pos[1]
-            dy_pos = y_pos[0] - y_pos[1]
-            dx = (dx_pos-1)*self.marker_real_gap/self.marker_real_width + 1
-            dy = (dy_pos-1)*self.marker_real_gap/self.marker_real_width + 1
+            dx_pos = x_pos[1] - x_pos[0]
+            dy_pos = y_pos[1] - y_pos[0]
+            dx = np.sign(dx_pos)*(np.abs(dx_pos)-1)*self.marker_real_gap/self.marker_real_width + 1
+            dy = np.sign(dy_pos)*(np.abs(dy_pos)-1)*self.marker_real_gap/self.marker_real_width + 1
             phi = np.arctan2(dy, dx)
             theta = np.arctan2(centers[1][1] - centers[0][1], centers[1][0] - centers[0][0])
             self.angle = theta - phi

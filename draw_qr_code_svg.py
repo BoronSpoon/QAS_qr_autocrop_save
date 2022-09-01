b50243e1cbf7f17b664b0f394989f4b427615d37
rayanti <rayanticlimatic@gmail.com>
2022-09-01 16:10:59 +0900

    svg does not support circular qr code

--- draw_qr_code_svg.py
+++ draw_qr_code_svg.py
@@ -3,9 +3,9 @@ import numpy as np
 import cv2
 import os
 import qrcode.image.svg
+from qrcode.image.styles.moduledrawers import SquareModuleDrawer, CircleModuleDrawer
 
-def draw_and_save_qrcode(version=4, box_size=4, string=""):
-    cwd = os.path.dirname(__file__)
+def draw_and_save_qrcode(version=4, box_size=4, string="", shape="square", path=None):
     qr = qrcode.QRCode(
         version=version,
         error_correction=qrcode.constants.ERROR_CORRECT_Q,
@@ -15,9 +15,15 @@ def draw_and_save_qrcode(version=4, box_size=4, string=""):
     )
     qr.add_data(string)
     qr.make(fit=True)
-    img = qr.make_image(fill_color=(255,255,255), back_color=(0,0,0))
-    with open(os.path.join(cwd, "test", "1.svg"), "wb") as f:
+    if shape is None:
+        img = qr.make_image(fill_color=(255,255,255), back_color=(0,0,0))
+    elif shape is not None:
+        img = qr.make_image(fill_color=(255,255,255), back_color=(0,0,0), module_drawer=shape)
+    with open(path, "wb") as f:
         img.save(f)
 
 if __name__ == "__main__":
-    draw_and_save_qrcode(version=4, box_size=4, string="test123")
+    cwd = os.path.dirname(__file__)
+    print(dir(qrcode.image.styles.moduledrawers))
+    draw_and_save_qrcode(version=4, box_size=4, string="test123", path=os.path.join(cwd, "test", "1.svg"))
+    draw_and_save_qrcode(version=4, box_size=4, string="test123", shape=CircleModuleDrawer, path=os.path.join(cwd, "test", "2.svg"))
---
+++ parse_qr_code_svg.py
@@ -0,0 +1,29 @@
+import os, sys
+import numpy as np
+cwd = os.path.dirname(__file__)
+sys.path.append(os.path.join(cwd, "..", "autocad","autocadshapes"))
+from svgpathtools import svg2paths, wsvg
+import svgpathtools
+from basic_shapes import polyline
+layer = "Ag%0_LPC_"
+
+# square qr code
+def draw_qr_code_in_cad(path):
+    lines, attributes = svg2paths(path)
+    polyline_points = []
+    for line in lines[0]:
+        if type(line) == svgpathtools.path.Line:
+            start = [np.real(line.start), np.imag(line.start)]
+            end = [np.real(line.end), np.imag(line.end)]
+            polyline_points.append(start)
+            if end == polyline_points[0]: # if polyline is closed
+                polyline(polyline_points, layer) # draw polyline and empty polyline list
+                polyline_points = []
+        elif type(line) == svgpathtools.path.Curve:
+            pass
+
+if __name__ == "__main__":
+    # square
+    draw_qr_code_in_cad("test/1.svg")
+    # circular
+    draw_qr_code_in_cad("test/2.svg")

c1f9108c8d4e0b7b1cb399c91827cadbfd425b0a
rayanti <rayanticlimatic@gmail.com>
2022-09-01 16:47:13 +0900

    trying svg decode

--- draw_qr_code_svg.py
+++ draw_qr_code_svg.py
@@ -5,7 +5,7 @@ import os
 import qrcode.image.svg
 from qrcode.image.styles.moduledrawers import SquareModuleDrawer, CircleModuleDrawer
 
-def draw_and_save_qrcode(version=4, box_size=4, string="", shape="square", path=None):
+def draw_and_save_qrcode(version=4, box_size=4, string="", path=None):
     qr = qrcode.QRCode(
         version=version,
         error_correction=qrcode.constants.ERROR_CORRECT_Q,
@@ -15,15 +15,11 @@ def draw_and_save_qrcode(version=4, box_size=4, string="", shape="square", path=
     )
     qr.add_data(string)
     qr.make(fit=True)
-    if shape is None:
-        img = qr.make_image(fill_color=(255,255,255), back_color=(0,0,0))
-    elif shape is not None:
-        img = qr.make_image(fill_color=(255,255,255), back_color=(0,0,0), module_drawer=shape)
-    with open(path, "wb") as f:
-        img.save(f)
+    img = str(qr.make_image(fill_color=(255,255,255), back_color=(0,0,0)).to_string)
+    with open(path, "w") as f:
+        img = str.replace(img, "mm", "") # remove units
+        f.write(img[2:-1]) # remove b''
 
 if __name__ == "__main__":
     cwd = os.path.dirname(__file__)
-    print(dir(qrcode.image.styles.moduledrawers))
     draw_and_save_qrcode(version=4, box_size=4, string="test123", path=os.path.join(cwd, "test", "1.svg"))
-    draw_and_save_qrcode(version=4, box_size=4, string="test123", shape=CircleModuleDrawer, path=os.path.join(cwd, "test", "2.svg"))

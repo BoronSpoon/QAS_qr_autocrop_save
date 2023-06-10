67a94ca408aecbc6d1255f7d607b783520f5efef
BoronSpoon <40061757+BoronSpoon@users.noreply.github.com>
2023-06-10 12:35:17 +0900

    upgrade to opencv-contrib-python 47 with tilt detection

--- wechat_qr_code_test.py
+++ wechat_qr_code_test.py
@@ -2,14 +2,13 @@ import cv2
 import sys
 import time
 import os
+import numpy as np
 cwd = os.path.dirname(__file__)
 
-def displayBbox(im, bbox):
-    if bbox is not None:
-        bbox = [bbox[0].astype(int)]
-        n = len(bbox[0])
-        for i in range(n):
-            cv2.line(im, tuple(bbox[0][i]), tuple(bbox[0][(i+1) % n]), (0,255,0), 3)
+def displayBbox(im, bboxes):
+    if bboxes is not None:
+        bboxes = [bbox.astype(np.int32) for bbox in bboxes]
+        cv2.polylines(im, bboxes, True, (0,0,255), 3)
 
 if __name__ == '__main__':
     detector = cv2.wechat_qrcode_WeChatQRCode(
@@ -21,6 +20,7 @@ if __name__ == '__main__':
     
     #img = cv2.imread(os.path.join(cwd, "test", "test_qr.png"))
     img = cv2.imread(os.path.join(cwd, "test", "2.png"))
+    img = np.where(img<200, 255, 0).astype(np.uint8)
     #cv2.imshow("a", img)
     #cv2.waitKey(0)
  
@@ -36,7 +36,8 @@ if __name__ == '__main__':
         displayBbox(img, points)
     else:
         print('QRCode not detected')
- 
+
+    img = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
     cv2.imshow("Image", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()

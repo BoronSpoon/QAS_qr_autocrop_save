07e906028c5a599da9a5896d3e2c898e82aa8951
BoronSpoon <40061757+BoronSpoon@users.noreply.github.com>
2023-06-23 23:08:03 +0900

    inp

--- add_encode_decode_devices.py
+++ add_encode_decode_devices.py
@@ -1,50 +1,49 @@
 class AED_Devices():
     def __init__():
         pass
-    def encode_qrs(
-        self,
-    ):
-        pass
     def encode_qr(
         self,
-        qr_code_type = 0,
+        qr_code_type_count = 6,
         **kwargs,
     ):
+        strings = {i:[] for i in range(qr_code_type_count)}
         string = ""
-        if qr_code_type == 0:
-            string = f'{qr_code_type};{kwargs["total_device_count"]},{kwargs["operator_name"]},{kwargs["chip_name"]}'
-        elif qr_code_type == 1:
-            string = f'{qr_code_type};'
-            arg_dicts = kwargs["arg_dicts"]
-            for i in range(len(arg_dicts)):
-                arg_dict = arg_dicts[i]
-                string += f'{arg_dict["folder_depth_count"]},{arg_dict["start_index"]},'
-                string += f'{",".join([i for i in arg_dict["folder_names"]])};'
-        elif qr_code_type == 2:
-            string = f'{qr_code_type};'
-            arg_dicts = kwargs["arg_dicts"]
-            for i in range(len(arg_dicts)):
-                arg_dict = arg_dicts[i]
-                string += f'{arg_dict["start_device_count"]},{arg_dict["end_device_count"]},{arg_dict["start_aruco_count"]},{arg_dict["end_aruco_count"]},'
-                string += f'{arg_dict["device_x_len"]},{arg_dict["device_y_len"]},{arg_dict["aruco_x_offset"]},{arg_dict["aruco_y_offset"]},'
-                string += f'{arg_dict["aruco_size"]},'
-                string += f'{",".join([i for i in arg_dict["folder_count_at_each_depth"]])};'
-        elif qr_code_type == 3:
-            string = f'{qr_code_type};{kwargs["process_count"]},{kwargs["process_name"]}'
-        elif qr_code_type == 4:
-            string = f'{qr_code_type},{kwargs["process_count"]};'
-            arg_dicts = kwargs["arg_dicts"]
-            for i in range(len(arg_dicts)):
-                arg_dict = arg_dicts[i]
-                string += f'{arg_dict["folder_depth_count"]},{arg_dict["start_index"]},'
-                string += f'{",".join([i for i in arg_dict["folder_names"]])};'
-        elif qr_code_type == 5:
-            string = f'{qr_code_type},{kwargs["process_count"]};'
-            arg_dicts = kwargs["arg_dicts"]
-            for i in range(len(arg_dicts)):
-                arg_dict = arg_dicts[i]
-                string += f'{arg_dict["start_device_count"]},{arg_dict["end_device_count"]},'
-                string += f'{",".join([i for i in arg_dict["folder_count_at_each_depth"]])};'
+        for qr_code_type in range(qr_code_type_count):
+            if qr_code_type == 0:
+                string = f'{qr_code_type};{kwargs["total_device_count"]},{kwargs["operator_name"]},{kwargs["chip_name"]}'
+                strings[qr_code_type].append(string)
+            elif qr_code_type == 1:
+                arg_dicts = kwargs["arg_dicts"]
+                for i in range(len(arg_dicts)):
+                    string = f'{qr_code_type};'
+                    arg_dict = arg_dicts[i]
+                    string += f'{arg_dict["folder_depth_count"]},{arg_dict["start_index"]},'
+                    string += f'{",".join([i for i in arg_dict["folder_names"]])};'
+            elif qr_code_type == 2:
+                string = f'{qr_code_type};'
+                arg_dicts = kwargs["arg_dicts"]
+                for i in range(len(arg_dicts)):
+                    arg_dict = arg_dicts[i]
+                    string += f'{arg_dict["start_device_count"]},{arg_dict["end_device_count"]},{arg_dict["start_aruco_count"]},{arg_dict["end_aruco_count"]},'
+                    string += f'{arg_dict["device_x_len"]},{arg_dict["device_y_len"]},{arg_dict["aruco_x_offset"]},{arg_dict["aruco_y_offset"]},'
+                    string += f'{arg_dict["aruco_size"]},'
+                    string += f'{",".join([i for i in arg_dict["folder_count_at_each_depth"]])};'
+            elif qr_code_type == 3:
+                string = f'{qr_code_type};{kwargs["process_count"]},{kwargs["process_name"]}'
+            elif qr_code_type == 4:
+                string = f'{qr_code_type},{kwargs["process_count"]};'
+                arg_dicts = kwargs["arg_dicts"]
+                for i in range(len(arg_dicts)):
+                    arg_dict = arg_dicts[i]
+                    string += f'{arg_dict["folder_depth_count"]},{arg_dict["start_index"]},'
+                    string += f'{",".join([i for i in arg_dict["folder_names"]])};'
+            elif qr_code_type == 5:
+                string = f'{qr_code_type},{kwargs["process_count"]};'
+                arg_dicts = kwargs["arg_dicts"]
+                for i in range(len(arg_dicts)):
+                    arg_dict = arg_dicts[i]
+                    string += f'{arg_dict["start_device_count"]},{arg_dict["end_device_count"]},'
+                    string += f'{",".join([i for i in arg_dict["folder_count_at_each_depth"]])};'
         
         return string
     

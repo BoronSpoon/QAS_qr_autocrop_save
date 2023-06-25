70f3e267e7cb0ff1379f58f91bab71f26dc0995a
BoronSpoon <40061757+BoronSpoon@users.noreply.github.com>
2023-06-25 19:07:25 +0900

    inp

--- add_encode_devices.py
+++ add_encode_devices.py
@@ -34,31 +34,43 @@ class AED_Devices():
                             string = ""
                             continue
                         else:
-                            string += string
+                            string += string_
                             i += 1
             elif qr_code_type == 2:
                 i = 0
                 j = 0
-                string = ""
+                string_list = []
+                string_len_list = []
                 for i in range(len(self.devices)):
-                    while (j < len(self.device_aruco)):
-                        if string == "":
-                            string += f'{qr_code_type};{i},{i+1},'
-                            string += f'{j},{j+1},'
-                        arg_dict = arg_dicts[i]
-                        string_ = ""
-                        string_ += f'{self.devices[i]["device_x_len"]},{self.devices[i]["device_y_len"]},'
-                        string_ += f'{self.device_aruco[i][j]["aruco_x_offset"]},{self.device_aruco[i][j]["aruco_y_offset"]},'
-                        string_ += f'{self.device_aruco[i][j]["aruco_size"]},'
-                        string_ += f'{",".join([i for i in self.devices[i]["folder_count_at_each_depth"]])};'
-                    
-                        if len(string + string_) > char_count_limit:
+                    for j in range(len(self.device_aruco)):
+                        string = ""
+                        string += f'{qr_code_type};{i},{i+1},'
+                        string += f'{j},{j+1},'
+                        string += f'{self.devices[i]["device_x_len"]},{self.devices[i]["device_y_len"]},'
+                        string += f'{self.device_aruco[i][j]["aruco_x_offset"]},{self.device_aruco[i][j]["aruco_y_offset"]},'
+                        string += f'{self.device_aruco[i][j]["aruco_size"]},'
+                        string += f'{",".join([i for i in self.devices[i]["folder_count_at_each_depth"]])};'
+                        string_list.append(string)
+                        string_len_list.append(len(string))
+
+                total_string_len = 0
+                string = ""
+                for i in range(len(string_len_list)):
+                    string_ = string_list[i]
+                    total_string_len += string_len_list[i]
+                    if i == len(string_len_list)-1: # last element
+                        if total_string_len > char_count_limit: # not within char_count_limit, split and append individually
                             strings[qr_code_type].append(string)
-                            string = ""
-                            continue
+                            strings[qr_code_type].append(string_)
+                        else: # within char_count_limit
+                            strings[qr_code_type].append(string + string_)
+                    else: 
+                        if total_string_len > char_count_limit:  # not within char_count_limit, split and append the one within limit
+                            strings[qr_code_type].append(string)
+                            string = string_
                         else:
-                            string += string
-                            i += 1
+                            string += string_
+
             elif qr_code_type == 3:
                 for process_count, process in enumerate(self.processes):
                     string = f'{qr_code_type};{process_count},{process["name"]}'
@@ -66,41 +78,45 @@ class AED_Devices():
             elif qr_code_type == 4:
                 i = 0
                 string = ""
-                arg_dicts = kwargs["arg_dicts"]
-                while (i < len(arg_dicts)):
-                    if string == "":
-                        string = f'{qr_code_type};'
-                    arg_dict = arg_dicts[i]
-                    string_ = ""
-                    string_ += f'{arg_dict["folder_depth_count"]},{arg_dict["start_index"]},'
-                    string_ += f'{",".join([i for i in arg_dict["folder_names"]])};'
+                for folder_depth_count, folder_names in self.process_folder_names.keys():
+                    while (i < len(folder_names)):
+                        if string == "":
+                            string = f'{qr_code_type};{folder_depth_count},{i},'
+                        string_ = f',{folder_names[i]}'
 
-                    if len(string + string_) > char_count_limit:
-                        strings[qr_code_type].append(string)
-                        string = ""
-                        continue
-                    else:
-                        string += string
-                        i += 1
+                        if len(string + string_) + 1 > char_count_limit:
+                            strings[qr_code_type].append(f"{string};")
+                            string = ""
+                            continue
+                        else:
+                            string += string_
+                            i += 1
             elif qr_code_type == 5:
                 i = 0
                 string = ""
-                arg_dicts = kwargs["arg_dicts"]
-                while (i < len(arg_dicts)):
-                    if string == "":
-                        string = f'{qr_code_type};'
-                    arg_dict = arg_dicts[i]
-                    string_ = ""
-                    string_ += f'{arg_dict["start_device_count"]},{arg_dict["end_device_count"]},'
-                    string_ += f'{",".join([i for i in arg_dict["folder_count_at_each_depth"]])};'
+                for i in range(len(self.devices)):
+                    string += f'{qr_code_type};{i},{i+1},'
+                    string += f'{",".join([i for i in self.devices[i]["folder_count_at_each_depth"]])};'
+                    string_list.append(string)
+                    string_len_list.append(len(string))
 
-                    if len(string + string_) > char_count_limit:
-                        strings[qr_code_type].append(string)
-                        string = ""
-                        continue
-                    else:
-                        string += string
-                        i += 1
+                total_string_len = 0
+                string = ""
+                for i in range(len(string_len_list)):
+                    string_ = string_list[i]
+                    total_string_len += string_len_list[i]
+                    if i == len(string_len_list)-1: # last element
+                        if total_string_len > char_count_limit: # not within char_count_limit, split and append individually
+                            strings[qr_code_type].append(string)
+                            strings[qr_code_type].append(string_)
+                        else: # within char_count_limit
+                            strings[qr_code_type].append(string + string_)
+                    else: 
+                        if total_string_len > char_count_limit:  # not within char_count_limit, split and append the one within limit
+                            strings[qr_code_type].append(string)
+                            string = string_
+                        else:
+                            string += string_
         
         return strings
     

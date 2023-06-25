0e886e0d9e2a484645781fb2e7b8c36e6a2e0889
BoronSpoon <40061757+BoronSpoon@users.noreply.github.com>
2023-06-25 19:49:06 +0900

    inp

--- add_encode_devices.py
+++ add_encode_devices.py
@@ -19,8 +19,10 @@ class AED_Devices():
         string = ""
         for qr_code_type in range(qr_code_type_count):
             if qr_code_type == 0:
-                string = f'{qr_code_type};{len(self.devices)},{self.operator_name},{self.chip_name}'
-                strings[qr_code_type].append(string)
+                strings[qr_code_type].append(
+                    f'{qr_code_type};{len(self.devices)},{self.operator_name},{self.chip_name}'    
+                )
+
             elif qr_code_type == 1:
                 for folder_depth_count, folder_names in self.device_folder_names.keys():
                     i = 0
@@ -36,45 +38,47 @@ class AED_Devices():
                         else:
                             string += string_
                             i += 1
+
             elif qr_code_type == 2:
                 i = 0
                 j = 0
-                string_list = []
-                string_len_list = []
+                accumulated_string_len = 0
+                accumulated_string = ""
+                string_header = f"{qr_code_type};"
+                string_header_len = len(string_header)
                 for i in range(len(self.devices)):
                     for j in range(len(self.device_aruco)):
-                        string = ""
-                        string += f'{qr_code_type};{i},{i+1},'
-                        string += f'{j},{j+1},'
-                        string += f'{self.devices[i]["device_x_len"]},{self.devices[i]["device_y_len"]},'
-                        string += f'{self.device_aruco[i][j]["aruco_x_offset"]},{self.device_aruco[i][j]["aruco_y_offset"]},'
-                        string += f'{self.device_aruco[i][j]["aruco_size"]},'
-                        string += f'{",".join([i for i in self.devices[i]["folder_count_at_each_depth"]])};'
-                        string_list.append(string)
-                        string_len_list.append(len(string))
+                        current_string = "".join([
+                            f'{i},{i+1},',
+                            f'{j},{j+1},',
+                            f'{self.devices[i]["device_x_len"]},{self.devices[i]["device_y_len"]},',
+                            f'{self.device_aruco[i][j]["aruco_x_offset"]},{self.device_aruco[i][j]["aruco_y_offset"]},',
+                            f'{self.device_aruco[i][j]["aruco_size"]},',
+                            f'{",".join([i for i in self.devices[i]["folder_count_at_each_depth"]])};',
+                        ])
+                        current_string_len = len(current_string)
 
-                total_string_len = 0
-                string = ""
-                for i in range(len(string_len_list)):
-                    string_ = string_list[i]
-                    total_string_len += string_len_list[i]
-                    if i == len(string_len_list)-1: # last element
-                        if total_string_len > char_count_limit: # not within char_count_limit, split and append individually
-                            strings[qr_code_type].append(string)
-                            strings[qr_code_type].append(string_)
+                    if i == len(self.devices)-1 and j == len(self.device_aruco)-1: # last element
+                        if accumulated_string_len + current_string_len > char_count_limit: # not within char_count_limit, split and append individually
+                            strings[qr_code_type].append(string_header + accumulated_string)
+                            strings[qr_code_type].append(string_header + current_string)
                         else: # within char_count_limit
-                            strings[qr_code_type].append(string + string_)
+                            strings[qr_code_type].append(string_header + accumulated_string + current_string)
                     else: 
-                        if total_string_len > char_count_limit:  # not within char_count_limit, split and append the one within limit
-                            strings[qr_code_type].append(string)
-                            string = string_
+                        if accumulated_string_len + current_string_len > char_count_limit:  # not within char_count_limit, split and append the one within limit
+                            strings[qr_code_type].append(string_header + accumulated_string)
+                            accumulated_string_len = current_string_len
+                            accumulated_string = current_string
                         else:
-                            string += string_
+                            accumulated_string_len += current_string_len
+                            accumulated_string += current_string
 
             elif qr_code_type == 3:
                 for process_count, process in enumerate(self.processes):
-                    string = f'{qr_code_type};{process_count},{process["name"]}'
-                    strings[qr_code_type].append(string)
+                    strings[qr_code_type].append(
+                        f'{qr_code_type};{process_count},{process["name"]}'
+                    )
+
             elif qr_code_type == 4:
                 i = 0
                 string = ""
@@ -91,34 +95,38 @@ class AED_Devices():
                         else:
                             string += string_
                             i += 1
-            elif qr_code_type == 5:
-                i = 0
-                string = ""
-                for i in range(len(self.devices)):
-                    string += f'{qr_code_type};{i},{i+1},'
-                    string += f'{",".join([i for i in self.devices[i]["folder_count_at_each_depth"]])};'
-                    string_list.append(string)
-                    string_len_list.append(len(string))
 
-                total_string_len = 0
-                string = ""
-                for i in range(len(string_len_list)):
-                    string_ = string_list[i]
-                    total_string_len += string_len_list[i]
-                    if i == len(string_len_list)-1: # last element
-                        if total_string_len > char_count_limit: # not within char_count_limit, split and append individually
-                            strings[qr_code_type].append(string)
-                            strings[qr_code_type].append(string_)
-                        else: # within char_count_limit
-                            strings[qr_code_type].append(string + string_)
-                    else: 
-                        if total_string_len > char_count_limit:  # not within char_count_limit, split and append the one within limit
-                            strings[qr_code_type].append(string)
-                            string = string_
+            elif qr_code_type == 5:
+                i = 0                
+                accumulated_string_len = 0
+                accumulated_string = ""
+                for j in range(len(self.devices[0])): # number of processes
+                    for i in range(len(self.devices)): # number of devices
+                        if i == 0: # create new QR code at new process
+                            string_header = f"{qr_code_type},{j};"
+                            string_header_len = len(string_header)
                         else:
-                            string += string_
+                            current_string = "".join([
+                                f'{i},{i+1},',
+                                f'{",".join([i for i in self.processes[i][j]["folder_count_at_each_depth"]])};',
+                            ])
+                            current_string_len = len(current_string)
+                        if j == len(self.devices[0])-1 and i == len(self.devices)-1: # last element
+                            if string_header_len + accumulated_string_len + current_string_len > char_count_limit: # not within char_count_limit, split and append individually
+                                strings[qr_code_type].append(string_header + accumulated_string)
+                                strings[qr_code_type].append(string_header + current_string)
+                            else: # within char_count_limit
+                                strings[qr_code_type].append(string_header + accumulated_string + current_string)
+                        else: 
+                            if string_header_len + accumulated_string_len + current_string_len > char_count_limit:  # not within char_count_limit, split and append the one within limit
+                                strings[qr_code_type].append(string_header + accumulated_string)
+                                accumulated_string_len = current_string_len
+                                accumulated_string = current_string
+                            else:
+                                accumulated_string_len += current_string
+                                accumulated_string += current_string
         
         return strings
     
     if __name__ == "__main__":
-        string = encode_qr(qr_code_type = 0)
+        strings = encode_qr(qr_code_type = 0)

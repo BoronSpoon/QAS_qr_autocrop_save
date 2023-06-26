28f05ba5d6d1e3c7b954a5656f4d08e656b9445c
BoronSpoon <40061757+BoronSpoon@users.noreply.github.com>
2023-06-26 16:43:55 +0900

    debug done

--- decode_devices.py
+++ decode_devices.py
@@ -17,6 +17,7 @@ class DecodeDevices():
     ):
         arg_dicts = string.split(";")
         qr_code_type = int(arg_dicts[0].split(",")[0])
+
         if qr_code_type == 0:
             args = arg_dicts[1].split(",")
             self.total_device_count = int(args[0])
@@ -73,7 +74,7 @@ class DecodeDevices():
                 if arg_dict != "":
                     process_count = int(arg_dict[0])
                     process_name  =     arg_dict[1]
-                    self.processes[process_count] = process_name
+                    self.process_names[process_count] = process_name
 
         elif qr_code_type == 4:
             process_count = int(arg_dicts[0].split(",")[1])
@@ -87,21 +88,21 @@ class DecodeDevices():
                     folder_names       =     args[2:]
                     if folder_depth_count not in self.process_folder_names[process_count].keys():
                         self.process_folder_names[process_count][folder_depth_count] = {}
-                    for folder_name in range(start_index,start_index+len(folder_names)):
-                        self.process_folder_names[process_count][folder_depth_count][folder_name] = folder_names
+                    for index in range(len(folder_names)):
+                        self.process_folder_names[process_count][folder_depth_count][start_index + index] = folder_names[index]
 
         elif qr_code_type == 5:
             process_count = int(arg_dicts[0].split(",")[1])
-            if process_count not in self.process_folder_names.keys():
-                self.process_folder_names[process_count] = {}
+            if process_count not in self.processes.keys():
+                self.processes[process_count] = {}
             for i, arg_dict in enumerate(arg_dicts[1:]):
                 if arg_dict != "":
                     args = arg_dict.split(",")
                     start_device_count         = int(args[0])
                     end_device_count           = int(args[1])
-                    folder_count_at_each_depth =     args[2:]
+                    folder_count_at_each_depth = [int(i) for i in args[2:]]
                     for device_count in range(start_device_count,end_device_count):
-                        self.process_folder_names[process_count][device_count] = [self.process_folder_names[i][folder_count] for (i, folder_count) in enumerate(folder_count_at_each_depth)]
+                        self.processes[process_count][device_count] = [self.process_folder_names[i][folder_count] for (i, folder_count) in enumerate(folder_count_at_each_depth)]
         
         return string
 

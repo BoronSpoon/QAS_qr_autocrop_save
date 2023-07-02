4c449d8fbcc6e33f589c86ce481f02d6f5555402
BoronSpoon <40061757+BoronSpoon@users.noreply.github.com>
2023-07-02 20:55:35 +0900

    inp

--- draw_aruco.py
+++ draw_aruco.py
@@ -1,21 +1,21 @@
 import numpy as np
 import os
 import cv2
-from draw_qr import *
+from draw_qrcode import *
 
-def get_aruco_pixels(dictionary=cv2.aruco.DICT_4X4_1000, id=0):
+def get_aruco_pixels(id=0, dictionary=cv2.aruco.DICT_4X4_1000):
     dictionary = cv2.aruco.getPredefinedDictionary(dictionary)
     bits_count = dictionary.markerSize+2
     data = cv2.aruco.generateImageMarker(dictionary, id, sidePixels=bits_count) # add borderbits
     return data
 
-def draw_aruco_cv2(px_size=10, dictionary=cv2.aruco.DICT_4X4_1000, id=0, data=None):
+def draw_aruco_cv2(px_size=10, id=0, dictionary=cv2.aruco.DICT_4X4_1000, data=None):
     if data is None:
         data = get_aruco_pixels(dictionary=dictionary, id=id)
     frame = cv2.resize(255-data*255, (0, 0), fx=px_size, fy=px_size, interpolation=0)
     return frame
 
-def draw_aruco_polylines(xmin, ymin, px_size, dictionary=cv2.aruco.DICT_4X4_1000, id=0, data=None):
+def draw_aruco_polylines(xmin, ymin, px_size, id=0, dictionary=cv2.aruco.DICT_4X4_1000, data=None):
     if data is None:
         data = get_aruco_pixels(dictionary=dictionary, id=id)
     polylines = []
